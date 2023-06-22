0ec2548bd7a848e3f3e28fb9aa7850e1c1fd4b6e
novitasalsabila <novnovv10@gmail.com>
2023-06-22 16:08:50 +0700

    jika front position<=REAR_position,iterasi dari front hingga rear

--- CircullarQueues_136/CircullarQueues_136.cpp
+++ CircullarQueues_136/CircullarQueues_136.cpp
@@ -67,4 +67,11 @@ public:
         }
         cout << "\nElements in the queue are...\n";
 
-     
+        //jika front position<=REAR_position,iterasi dari front hingga rear
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
