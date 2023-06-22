8f474d57eee3d1d29a305bc806515eb77b693e69
novitasalsabila <novnovv10@gmail.com>
2023-06-22 16:11:24 +0700

    Iterasi dari awal array hingga rear

--- CircullarQueues_136/CircullarQueues_136.cpp
+++ CircullarQueues_136/CircullarQueues_136.cpp
@@ -81,4 +81,11 @@ public:
             cout << queue_array[FRONT_position] << "   ";
         }
         FRONT_position = 0;
-       
+        //Iterasi dari awal array hingga rear
+        while (FRONT_position <= REAR_position) {
+            cout << queue_array[FRONT_position] << "   ";
+            FRONT_position++;
+        }
+        cout << endl;
+    }
+};
